af49a958006bf310284541838c9f2a2888419e91
loviewer <tomatoo3o97@gmail.com>
2023-04-07 15:32:26 +0900

    프로젝트 파일 재생성

---
+++ side_back/src/test/java/com/communityboard/side_back/SideBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.communityboard.side_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SideBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
