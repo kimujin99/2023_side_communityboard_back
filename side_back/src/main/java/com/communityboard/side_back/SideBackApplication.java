aca3704f14d02a1ad6bb23799256dea7cb58d99f
KUJ <87636036+kimujin99@users.noreply.github.com>
2023-04-07 15:15:27 +0900

    프로젝트 파일 업로드

---
+++ side_back/src/main/java/com/communityboard/side_back/SideBackApplication.java
@@ -0,0 +1,13 @@
+package com.communityboard.side_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SideBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SideBackApplication.class, args);
+	}
+
+}
