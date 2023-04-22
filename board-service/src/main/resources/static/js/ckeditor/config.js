09736007941bae25d98134c04c589a981eb9c911
loviewer <tomatoo3o97@gmail.com>
2023-04-22 14:56:11 +0900

    CKEditor4 다운로드

---
+++ board-service/src/main/resources/static/js/ckeditor/config.js
@@ -0,0 +1,26 @@
+/**
+ * @license Copyright (c) 2003-2023, CKSource Holding sp. z o.o. All rights reserved.
+ * For licensing, see https://ckeditor.com/legal/ckeditor-oss-license
+ */
+
+CKEDITOR.editorConfig = function( config ) {
+	config.toolbarGroups = [
+		{ name: 'styles', groups: [ 'styles' ] },
+		{ name: 'clipboard', groups: [ 'undo', 'clipboard' ] },
+		{ name: 'forms', groups: [ 'forms' ] },
+		{ name: 'basicstyles', groups: [ 'basicstyles', 'cleanup' ] },
+		{ name: 'paragraph', groups: [ 'list', 'indent', 'blocks', 'align', 'bidi', 'paragraph' ] },
+		{ name: 'colors', groups: [ 'colors' ] },
+		{ name: 'insert', groups: [ 'insert' ] },
+		{ name: 'links', groups: [ 'links' ] },
+		{ name: 'tools', groups: [ 'tools' ] },
+		{ name: 'editing', groups: [ 'find', 'selection', 'spellchecker', 'editing' ] },
+		{ name: 'document', groups: [ 'document', 'mode', 'doctools' ] },
+		{ name: 'others', groups: [ 'others' ] },
+		{ name: 'about', groups: [ 'about' ] }
+	];
+
+	config.removeButtons = 'Save,NewPage,Print,Scayt,SelectAll,Form,Checkbox,Radio,TextField,Textarea,Select,Button,ImageButton,HiddenField,CreateDiv,Language,Flash,PageBreak,Iframe,Templates,Anchor,Unlink,Preview,Source,About,Styles,Subscript,Superscript,CopyFormatting,RemoveFormat,BidiLtr,BidiRtl,HorizontalRule,Maximize,ShowBlocks,Find,Replace,Outdent,Indent,Cut,Copy,Paste,PasteText,PasteFromWord';
+    config.resize_enabled = false;
+    config.height = 800;
+};
