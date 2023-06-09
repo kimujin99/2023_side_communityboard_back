90311464ffba155e80e71eecc8bbfbcdaee7b929
loviewer <tomatoo3o97@gmail.com>
2023-06-09 19:50:36 +0900

    톰캣 타임 존 설정

--- board-service/src/main/java/side/boardservice/BoardServiceApplication.java
+++ board-service/src/main/java/side/boardservice/BoardServiceApplication.java
@@ -1,7 +1,10 @@
 package side.boardservice;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.TimeZone;
 
 @SpringBootApplication
 public class BoardServiceApplication {
@@ -9,5 +12,9 @@ public class BoardServiceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(BoardServiceApplication.class, args);
 	}
+	@PostConstruct
+	public void started(){
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
 
+	}
 }
